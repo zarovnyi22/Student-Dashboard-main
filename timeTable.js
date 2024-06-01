111771f3a178258516e26da18082ff666a8db6d0
Nazar Zarovnyi <zarovnyin@MacBook-Pro-Nazar.local>
2024-06-01 09:47:40 +0300

    Initial commit

---
+++ timeTable.js
@@ -0,0 +1,133 @@
+const Sunday =[
+    {
+        time: 'Неділя',
+        roomNumber: 'Вихідний',
+        subject: 'Немає доступних занять',
+        type: ''
+    }
+]
+const Monday =[
+    {
+        time: '09-10 РАНКУ',
+        roomNumber: '38-718',
+        subject: 'БДМС130',
+        type: 'Лекція'
+    },
+    {
+        time: '10-11 РАНКУ',
+        roomNumber: '38-718',
+        subject: 'МТН166',
+        type: 'Практичне заняття'
+    },
+    {
+        time: '12-01 ПОЛУДНЯ',
+        roomNumber: '38-718',
+        subject: 'НС200',
+        type: 'Лекція'
+    }
+]
+const Tuesday =[
+    {
+        time: '09-10 РАНКУ',
+        roomNumber: '27-304У',
+        subject: 'МТН166',
+        type: 'Практичне заняття'
+    },
+    {
+        time: '11-12 РАНКУ',
+        roomNumber: '28-107',
+        subject: 'КС849',
+        type: 'Лекція'
+    },
+    {
+        time: '12-01 ПОЛУДНЯ',
+        roomNumber: '28-107',
+        subject: 'КС849',
+        type: 'Лекція'
+    },
+    {
+        time: '02-03 ПОЛУДНЯ',
+        roomNumber: '38-718',
+        subject: 'НС200',
+        type: 'Лекція'
+    }
+]
+
+const Wednesday =[
+    {
+        time: '10-11 РАНКУ',
+        roomNumber: '33-309',
+        subject: 'БДМС130',
+        type: 'Лекція'
+    },
+    {
+        time: '11-12 РАНКУ',
+        roomNumber: '38-719',
+        subject: 'КС200',
+        type: 'Лекція'
+    }
+]
+
+const Thursday =[
+    {
+        time: '11-12 РАНКУ',
+        roomNumber: '33-309',
+        subject: 'МТН166',
+        type: 'Лекція'
+    },
+    {
+        time: '01-02 ПОЛУДНЯ',
+        roomNumber: '38-719',
+        subject: 'КС849',
+        type: 'Лекція'
+    },
+    {
+        time: '02-03 ПОЛУДНЯ',
+        roomNumber: '38-718',
+        subject: 'НС200',
+        type: 'Лекція'
+    }
+]
+
+const Friday =[
+    {
+        time: '10-11 РАНКУ',
+        roomNumber: '33-309',
+        subject: 'МЕК103',
+        type: 'Лекція'
+    },
+    {
+        time: '11-12 РАНКУ',
+        roomNumber: '33-309',
+        subject: 'МЕК103',
+        type: 'Лекція'
+    },
+    {
+        time: '02-03 ПОЛУДНЯ',
+        roomNumber: '33-601',
+        subject: 'КС849',
+        type: 'Практичне заняття'
+    },
+
+]
+
+const Saturday =[
+    {
+        time: '09-10 РАНКУ',
+        roomNumber: '34-604',
+        subject: 'БДМС130',
+        type: 'Практичне заняття'
+    },
+    {
+        time: '10-11 РАНКУ',
+        roomNumber: '34-604',
+        subject: 'БДМС130',
+        type: 'Лекція'
+    },
+    {
+        time: '01-02 ПОЛУДНЯ',
+        roomNumber: '33-309',
+        subject: 'МТН166',
+        type: 'Лекція'
+    }
+]
